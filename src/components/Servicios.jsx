0946adde5eab6e3f114b0ed0f533f3f1ea5bca97
lindaperea <linda.hp@hotmail.com>
2023-01-27 22:55:16 -0500

    cambios

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -13,7 +13,7 @@ const Servicios = () => {
                 <div className='foto-about '>
                     <h2>MODELOS DE WEB</h2>
                     <div className='models'>
-                    <a className='center-models' href="https://landingmodel.netlify.app/" alt="imagen landing">Landing Page</a>
+                    <a className='center-models j' href="https://landingmodel.netlify.app/" alt="imagen landing">Landing Page</a>
                     <a className='center-models' href="https://www.inmobiliariacya.com" alt="imagen landing">Inmobiliaria</a>
                     <a className='center-models' href="https://mirestaurant.netlify.app" alt="imagen landing">Restaurant</a>
                         
@@ -21,7 +21,7 @@ const Servicios = () => {
                 </div>
                 
             </div>
-                <div className='text-about' >
+                {/* <div className='text-about' >
                     <div className='center-contacto' >
                         <div className='links-about'>
                         <a href="https://wa.me/message/74UFOE7GRBTPN1">CONTACTAME</a>    
@@ -35,7 +35,7 @@ const Servicios = () => {
                             disfruta de realizar proyectos para ayudar a los grandes y pequeños negocios a digitalizarse, migrar de una manera sencilla. Me gusta los desafios, acostumbrada a ha realizar de manera organizada varias tareas si la situación lo requiere, la programación para mi es plasmar las ideas de los clientes en aplicativos faciles de usar y agradables a la vista para que sea una experiencia de usuario inolvidable.
                         </p>
                     </div>
-                </div>
+                </div> */}
 
             </div>
 
